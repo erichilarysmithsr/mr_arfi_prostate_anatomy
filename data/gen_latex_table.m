98d6b19ca3482aaf26a76ecac9c705a96b2f1e0a
Mark Palmeri <mlp6@duke.edu>
2013-09-13 18:25:54 -0400

    added axis data table

--- data/gen_latex_table.m
+++ data/gen_latex_table.m
@@ -15,3 +15,11 @@ for i=1:size(data,1),
 end;
 
 fclose(fid);
+
+fid = fopen('axis_data.tex','w');
+
+for i=1:size(data,1),
+    fprintf(fid,'%i & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f \\\\ \n',i,data(i,4)/10,data(i,5)/10,data(i,6)/10,data(i,7)/10,data(i,8)/10,data(i,9)/10,data(i,12)/10,data(i,13)/10,data(i,14)/10,data(i,15)/10,data(i,16)/10,data(i,17)/10);
+end;
+
+fclose(fid);
