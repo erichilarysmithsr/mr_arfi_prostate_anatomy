903889ce20cbfdacec4398beeaa0e01055b58482
Mark Palmeri <mlp6@duke.edu>
2013-09-14 22:30:24 -0400

    added figure of weight vs ellipsoidal volume

--- figs/corr_weight_vol.m
+++ figs/corr_weight_vol.m
@@ -21,6 +21,7 @@ function corr_weight_vol()
 
     gen_fig_n_fits(w,mr_total_vol,arfi_total_vol,'weight');
     gen_fig_n_fits(path_vols,mr_total_vol,arfi_total_vol,'pathVol');
+    gen_fig_n_fits(w,path_vols,path_vols,'path_vol_weight');
 
     system('convert_eps_to_pdf.sh');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -29,10 +30,18 @@ function gen_fig_n_fits(w,mr_total_vol,arfi_total_vol,metric)
     fs = 18;
     figure;
     hold on;
-    plot(w,mr_total_vol,'bx','MarkerSize',14,'LineWidth',3);
-    plot(w,arfi_total_vol,'go','MarkerSize',14,'LineWidth',3);
-    ylabel('Total Volume (cm^3)','FontSize',fs)
-    if(strcmp(metric,'weight')),
+    if(strcmp(metric,'path_vol_weight')),
+        plot(w,mr_total_vol,'bx','MarkerSize',14,'LineWidth',3);
+    else,
+        plot(w,mr_total_vol,'bx','MarkerSize',14,'LineWidth',3);
+        plot(w,arfi_total_vol,'go','MarkerSize',14,'LineWidth',3);
+    end;
+    if(strcmp(metric,'path_vol_weight')),
+        ylabel('Pathology Ellipsoidal Volume (cm^3)','FontSize',fs)
+    else,
+        ylabel('Total Volume (cm^3)','FontSize',fs)
+    end;
+    if(strcmp(metric,'weight') || strcmp(metric,'path_vol_weight')),
         xlabel('Prostate Weight (g)','FontSize',fs);
     elseif(strcmp(metric,'pathVol')),
         xlabel('Prostate Pathology Volume (cm^3)','FontSize',fs);
@@ -56,12 +65,20 @@ function gen_fig_n_fits(w,mr_total_vol,arfi_total_vol,metric)
 
     plot(w,mr_fit,'-b','LineWidth',3);
 
-    [arfi_fit,arfi_Rsq]=compute_linreg_Rsq(w,arfi_total_vol);
+    if(~strcmp(metric,'path_vol_weight')),
+        [arfi_fit,arfi_Rsq]=compute_linreg_Rsq(w,arfi_total_vol);
 
-    plot(w,arfi_fit,'-g','LineWidth',3);
+        plot(w,arfi_fit,'-g','LineWidth',3);
 
-    legend(sprintf('MR (R^2 = %.2f)',mr_Rsq),sprintf('ARFI (R^2 = %.2f)',arfi_Rsq),'Location','NorthWest');
-    legend boxoff;
+    end;
+
+    if(strcmp(metric,'path_vol_weight')),
+        legend(sprintf('R^2 = %.2f',mr_Rsq),'Location','NorthWest');
+        legend boxoff;
+    else,
+        legend(sprintf('MR (R^2 = %.2f)',mr_Rsq),sprintf('ARFI (R^2 = %.2f)',arfi_Rsq),'Location','NorthWest');
+        legend boxoff;
+    end;
 
     print('-depsc2',sprintf('corr_%s_vol.eps',metric));
     close;
