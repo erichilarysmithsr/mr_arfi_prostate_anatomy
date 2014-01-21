eb961efde52c7bca21c36322ada00114954a1cd9
Mark Palmeri <mlp6@duke.edu>
2014-01-21 09:50:03 -0500

    compile in /tmp

--- compile-n-view.sh
+++ compile-n-view.sh
@@ -1,1 +1,7 @@
-latexmk -bibtex -pdf -pvc -outdir=/tmp/latex mr_arfi_anatomy.tex
+tmp_latex = "/tmp/latex"
+
+if [ ! -d $tmp_latex ]; then
+    mkdir $tmp_latex
+fi
+
+latexmk -bibtex -pdf -pvc -outdir=$tmp_latex mr_arfi_anatomy.tex
